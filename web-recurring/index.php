d54c2ce533a5fcdcf5c02453683c065d2ace1ff3
prathmeshbijjargi <prathmeshbijjargi@gmail.com>
2021-11-20 16:48:41 +0530

    Added Web Razorpay Recurring integration

---
+++ web-recurring/index.php
@@ -0,0 +1,139 @@
+<?php 
+include('templates/header.php');
+?>
+<form name="razorpay_frm_payment" class="razorpay-frm-payment" id="razorpay-frm-payment" method="post">
+<input type="hidden" name="language" value="EN">
+<section class="showcase">
+  <div class="container">
+    <div class="pb-2 mt-4 mb-2 border-bottom">
+      <h2>Razorpay Payment Gateway Recurring Integration </h2>
+    </div>
+
+    <div class="row align-items-center">
+       <div class="form-group col-md-6">
+        <label for="inputEmail4">Amount</label>
+        <input type="text" class="form-control" id="amount" name="amount" placeholder="Amount" required>
+      </div>
+
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Full Name</label>
+        <input type="text" name="billing_name" class="form-control" id="billing-name"  Placeholder="Name" required> 
+      </div>
+  </div>
+
+    <div class="row align-items-center">
+       <div class="form-group col-md-6">
+        <label for="inputEmail4">Email</label>
+        <input type="email" name="billing_email"class="form-control" id="billing-email" Placeholder="Email" required>
+      </div>
+
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Api Key</label>
+        <input type="text" name="api_key" class="form-control" id="api_key" Placeholder="Api Key" required>
+      </div>
+  </div>
+      <div class="row align-items-center">
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Secret Key</label>
+         <input type="text" name="secret_key" class="form-control" id="secret_key"  Placeholder="Secret Key" required>
+      </div>
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-success mt-4 float-right" id="razor-pay-now"><i class="fa fa-credit-card" aria-hidden="true"></i> Pay</button>
+      </div>
+    </div>
+</div>
+</section>
+</form>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<script type="text/javascript">
+
+    jQuery(document).on('click', '#razor-pay-now', function (e) {
+      var total = jQuery('input#amount').val() * 100;
+      var name = jQuery('input#billing-name').val();
+      var currency_code_id = jQuery('input#currency').val();
+      var key_id =  jQuery('input#api_key').val();
+      var secret_key =  jQuery('input#secret_key').val();
+      var store_name = 'Razorpay';
+      var store_description = 'Payment';
+      var store_logo = 'https://d6xcmfyh68wv8.cloudfront.net/assets/razorpay-glyph.svg';
+      var email = jQuery('input#billing-email').val();
+      jQuery('.text-danger').remove();
+
+    if(name=="") {
+      jQuery('input#billing-name').after('<small class="text-danger">Please enter name.</small>');
+      return false;
+    }
+    if(email=="") {
+      jQuery('input#billing-email').after('<small class="text-danger">Please enter valid email.</small>');
+      return false;
+    }
+    if(total=="") {
+        jQuery('input#billing-name').after('<small class="text-danger">Please enter valid amount.</small>');
+        return false;
+    }
+    if(key_id=="") {
+        jQuery('input#billing-email').after('<small class="text-danger">Please enter valid key.</small>');
+        return false;
+    }
+        $.ajax({
+            type: "POST",
+            url: 'order.php',
+            data: {'key':key_id,'secret':secret_key,'amount':total,'email':email,'name':name},
+            success: function(response)
+            {
+                var result=JSON.parse(response)
+                console.log(result);
+                if(result.error){
+                    alert('Please contact integration team')
+                    document.getElementById("razorpay-frm-payment").reset()
+                }else {
+                    open_checkout(result)
+                }
+            }
+        });
+        function open_checkout(x){
+            var razorpay_options = {
+                key: key_id,
+                amount: total,
+                order_id:x.order_id,
+                recurring: 1,
+                customer_id:x.customer_id,
+                name: store_name,
+                description: store_description,
+                image: store_logo,
+                currency: currency_code_id,
+                prefill: {
+                    name: name,
+                    email: email
+                },
+                notes: {
+                    order_id: x.order_id
+                },
+                "handler": function (response){
+                    alert(response.razorpay_payment_id);
+                    alert(response.razorpay_order_id);
+                    alert(response.razorpay_signature);
+                    document.getElementById("razorpay-frm-payment").reset()
+                }
+            };
+            // obj
+            var objrzpv1 = new Razorpay(razorpay_options);
+            objrzpv1.on('payment.failed', function (response){
+                alert(response.error.code);
+                alert(response.error.description);
+                alert(response.error.source);
+                alert(response.error.step);
+                alert(response.error.reason);
+                alert(response.error.metadata.order_id);
+                alert(response.error.metadata.payment_id);
+            });
+            objrzpv1.open();
+            e.preventDefault();
+        }
+
+});
+</script>
---
+++ web-recurring/order.php
@@ -0,0 +1,78 @@
+<?php
+
+$key=$_POST['key'];
+$secret=$_POST['secret'];
+$amount=intval($_POST['amount']);
+$email=$_POST['email'];
+$name=$_POST['name'];
+
+//Create Customer
+    $data=[
+        "name"=>$name,
+        "contact"=>"9123456780",
+        "email"=>$email,
+        "fail_existing"=>"0",
+    ];
+    $data_string = json_encode($data);
+    $chi = curl_init('https://api.razorpay.com/v1/customers');
+    curl_setopt($chi, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($chi, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($chi, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($chi, CURLOPT_USERPWD, $key. ":" .$secret);
+
+    // execute!
+    $response = curl_exec($chi);
+
+    // do anything you want with your response
+    $responseCustomer = json_decode($response, true);
+
+    //Create Order
+
+    if($responseCustomer['id']) {
+
+        $data = [
+            "amount" => $amount,
+            "currency" => "INR",
+            "receipt" => "rcptid_11",
+            "customer_id" => $responseCustomer['id'],
+            "method" => "upi",
+            "token" => [
+                "max_amount" => 200000,
+                "frequency" => "as_presented"
+            ]
+        ];
+
+        $data_string = json_encode($data);
+        $ch = curl_init('https://api.razorpay.com/v1/orders');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_USERPWD, $key . ":" . $secret);
+
+        $response = curl_exec($ch);
+
+        $responseOrder = json_decode($response, true);
+
+        // close the connection, release resources used
+        curl_close($ch);
+    }else{
+    curl_close($chi);
+}
+
+
+    if(!isset($responseOrder['id']) || !isset($responseCustomer['id'])) {
+        $data = [
+            'error' => true
+        ];
+    } else {
+        $data = [
+            'order_id' => $responseOrder['id'],
+            'customer_id' => $responseCustomer['id'],
+        ];
+
+    }
+print_r(json_encode($data, true));
+
+
+
+
